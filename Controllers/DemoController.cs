3f28d00c94f983bb380f85fd9882b4e43f7642d2
MD. Jashim Uddin <ju.cu.acc@outlook.com>
2022-12-03 17:19:40 +0600

    Add project files.

---
+++ Controllers/DemoController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNet6WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DemoController : ControllerBase
+    {
+
+        [HttpGet]
+        public string Grettings()
+        {
+            return "Hello there!";
+        }
+    }
+}
---
+++ Models/APIDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DotNet6WebAPI.Models
+{
+    public class APIDbContext:DbContext
+    {
+        public APIDbContext(DbContextOptions options):base(options)
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
